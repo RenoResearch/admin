b30420b7a500d57d75d71a7e26a1ffaed95fd2d5
edtech-hub <adis3778@zohomail.in>
2026-03-17 22:41:29 +0800

    Admin build 2026.03.17.2231 - Ala carte pushes, group management, membership filtering, add-to-group, analytics tabs

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3616445836"
+    serviceWorkerVersion: "148728905"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "3033210c3d0d2b93f7731afe13967e1a",
-"version.json": "7ead3ba931846ff56561cdad1fe53e9e",
+const RESOURCES = {"flutter_bootstrap.js": "3af954eb0c2b8f9b1c6926569611cee8",
+"version.json": "37c22a7347e2cdde3858e339239fa07b",
 "index.html": "f4f060bd0532d92d493b2c80cc7af8ce",
 "/": "f4f060bd0532d92d493b2c80cc7af8ce",
 "CNAME": "237f0d4608a1e2758397a0d2f647dfb7",
-"main.dart.js": "c1eaaf917e4b08aa8910653e47ec4d82",
+"main.dart.js": "5869cec19800caa145c6210f98d27171",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
