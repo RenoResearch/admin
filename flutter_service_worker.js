df6bc87abf248a3114a98c163ae5e8d9ca06a2f0
edtech-hub <adis3778@zohomail.in>
2026-03-31 23:43:28 +0800

    Admin Console build 2026.03.31.2343 – 2026-03-31

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "d82b765e763078a1f761f69a7d32f520",
-"version.json": "c9d48b6502ec5e8868e99b1c70b24b96",
+const RESOURCES = {"flutter_bootstrap.js": "b3d4daff63420c287f95fe1f38974098",
+"version.json": "7dcaf988c75db94569ab6b5a39927259",
 "index.html": "f4f060bd0532d92d493b2c80cc7af8ce",
 "/": "f4f060bd0532d92d493b2c80cc7af8ce",
 "CNAME": "237f0d4608a1e2758397a0d2f647dfb7",
-"main.dart.js": "1ff6b11f726e79bb69e13bccfa437948",
+"main.dart.js": "ac58f079705c0d77b0214314bccd539c",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
