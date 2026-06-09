a40a774d6644abdf2be52981cb5390cd9b16780a
edtech-hub <adis3778@zohomail.in>
2026-06-09 14:12:51 +0530

    Admin Console build 2026.06.09.1412 – 2026-06-09

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1885458800"
+    serviceWorkerVersion: "90791580"
   }
 });
